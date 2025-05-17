0a48c2a12d78e4b698808be56e261cf7ad803b40
George <george.cains@adrenalinmedia.com.au>
2025-05-17 20:41:49 +1000

    Remove google-weather-api-check-curl.sh after filter-repo

--- google-weather-api-check-curl.sh
+++
@@ -1,3 +0,0 @@
-$uri = "https://weather.googleapis.com/v1/forecast/days:lookup?key=AIzaSyCEHCsRlpq19IzjvYtRvdPbMzmWRyAQuP0&location.latitude=-33.8688&location.longitude=151.2093&days=7&units=METRIC&languageCode=en"
-$response = Invoke-RestMethod -Uri $uri -Method Get
-$response
